4114ba7b0a6ba7c98c37b0230c852b332109ea4f
Andrey Belik <belika@hotmail.com>
2024-09-10 14:07:15 +0100

    small fix

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ terraform {
     required_providers {
         aws = {
         source  = "hashicorp/aws"
-        version = "~> 5.65.0"
+        version = "~> 5.65.0" 
         }
         kubernetes = {
         source  = "hashicorp/kubernetes"
