ca11be024687cb9e2bfb5de2af427b39468a01bc
Andrey Belik <belika@hotmail.com>
2024-09-10 17:39:04 +0100

    fixing

--- zbctl.tf
+++ zbctl.tf
@@ -2,7 +2,7 @@
 resource "kubernetes_job" "zeebe_cli" {
   metadata {
     name      = "zeebe-cli-job"
-    namespace = "default" # Change to your namespace if needed
+    namespace = "camunda" # Change to your namespace if needed
   }
 
   spec {
@@ -19,12 +19,11 @@ resource "kubernetes_job" "zeebe_cli" {
         container {
           name  = "zeebe-cli"
           image = "alpine:3.17"
-          command = ["/bin/sh", "-c", <<EOF
-            apk add --no-cache curl bash \
-            && curl -L https://github.com/camunda/zeebe/releases/download/8.2.5/zbctl-8.2.5-linux-amd64.tar.gz \
-            | tar -xz -C /usr/local/bin \
-            && zbctl --address zeebe-gateway.camunda.svc.cluster.local:26500 status
-            EOF
+          command: ["/bin/sh", "-c"]
+          args: [
+            "apk update && \
+            npm install -g zbctl && \
+            zbctl --address camunda-zeebe-gateway.camunda.svc.cluster.local:26500 --insecure status"
           ]
         }
       }
