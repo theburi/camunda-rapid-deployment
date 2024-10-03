30a3da8052009650f0e797eee55eaaf4aa7f5260
Andrey Belik <belika@hotmail.com>
2024-10-03 09:52:19 +0100

    Updating with Pring Boot pipelines

---
+++ terraform/workflow-deployment/spring_app.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_deployment" "spring_app" {
+  metadata {
+    name      = "spring-app"
+    namespace = "default"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "spring-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "spring-app"
+        }
+      }
+
+      spec {
+        container {
+          name  = "spring-app"
+          image = "my-spring-app:${var.image_tag}"  # The image tag from the GitHub Actions workflow
+
+          ports {
+            container_port = 8080
+          }
+
+          env {
+            name  = "SPRING_PROFILES_ACTIVE"
+            value = "prod"
+          }
+        }
+      }
+    }
+  }
+}
+
+variable "image_tag" {
+  description = "The tag of the Docker image to deploy"
+  type        = string
+}
