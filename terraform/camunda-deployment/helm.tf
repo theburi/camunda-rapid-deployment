336a82af4fe06b8d6d58811668485afad90ef634
Andrey Belik <belika@hotmail.com>
2024-10-08 12:06:30 +0100

    Upgradign to version 8.6

--- terraform/camunda-deployment/helm.tf
+++ terraform/camunda-deployment/helm.tf
@@ -2,12 +2,12 @@ resource "helm_release" "camunda-platform" {
   name       = "camunda"
   repository = "https://helm.camunda.io"
   chart      = "camunda-platform"
-  version    = "10.4.0"
+  version    = "11.0.0"
   namespace  = "camunda"
 
   create_namespace = true
 
-  values = [file("${path.module}/values-v8.5.yaml")]
+  values = [file("${path.module}/values-v8.6.yaml")]
 
   # Inline set values based on your original YAML configuration
   set {
