8bdc526b0da90de2b826424d91b1553be0cf012e
Andrey Belik <belika@hotmail.com>
2024-10-10 14:17:38 +0100

    adding secret

--- terraform/workflow-deployment/spring_app.tf
+++ terraform/workflow-deployment/spring_app.tf
@@ -38,14 +38,52 @@ resource "kubernetes_deployment" "spring_app" {
             name  = "ZEEBE_ADDRESS"
             value = "camunda-zeebe-gateway.camunda.svc.cluster.local:26500"
           }
+                    env {
+            name  = "ZEEBE_CLIENT_ID"
+            value = "zeebe"
+          }
+
+          env {
+            name  = "ZEEBE_AUTHORIZATION_SERVER_URL"
+            value = "http://camunda.local:18080/auth/realms/camunda-platform/protocol/openid-connect/token"
+          }
+
+          env {
+            name  = "ZEEBE_TOKEN_AUDIENCE"
+            value = "zeebe-api"
+          }
+          # Reference the secret for sensitive value
+          env {
+            name = "ZEEBE_CLIENT_SECRET"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.zeebe_auth.metadata[0].name
+                key  = "ZEEBE_CLIENT_SECRET"
+              }
+            }
+          }
         }
       }
     }
   }
+  depends_on = [ "zeebe_auth" ]
 }
 
 variable "image_tag" {
   description = "The tag of the Docker image to deploy"
   type        = string
   default     = "latest"
+}
+
+resource "kubernetes_secret" "zeebe_auth" {
+  metadata {
+    name      = "zeebe-auth"
+    namespace = "camunda"
+  }
+
+  data = {
+    ZEEBE_CLIENT_SECRET = "VmVyeUxvbmdTdHJpbmc="  # Base64 encoded value
+  }
+
+  type = "Opaque"
 }
