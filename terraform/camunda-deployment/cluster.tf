72e2adf979a643963e7d8a4f3d29afaea9f7033c
Andrey Belik <belika@hotmail.com>
2024-10-10 17:52:49 +0100

    Reverting to START

--- terraform/camunda-deployment/cluster.tf
+++ terraform/camunda-deployment/cluster.tf
@@ -10,22 +10,3 @@ module "eks_cluster" {
   cluster_service_ipv4_cidr = "10.190.0.0/16"
   cluster_node_ipv4_cidr    = "10.192.0.0/16"
 }
-
-# Add your SSO role to the aws-auth ConfigMap
-resource "kubernetes_config_map" "aws_auth" {
-  depends_on = [module.eks_cluster]
-
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = <<YAML
-- rolearn: arn:aws:iam::123302325581:role/AWSReservedSSO_SystemAdministrator_3272c85503826b83
-  username: andrey.belik
-  groups:
-    - system:masters
-YAML
-  }
-}
